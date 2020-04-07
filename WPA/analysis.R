62dd01fa262e39f05320ce4dfebeadc538c1b826
paulapivat <paul.apivat@gmail.com>
2020-04-07 13:29:25 +0700

    add df_join_data all assignments for all staff_id who did at least one medco

--- WPA/analysis.R
+++ WPA/analysis.R
@@ -36,6 +36,9 @@ colnames(multiple_medco)[2] <- 'num_medco'
 df_medco_join <- df_medco %>%
     inner_join(multiple_medco, by = 'staff_id')
 
+# create variable for length of assignment; diff_date
+df_medco_join$diff_date <- as.Date(as.character(df_medco_join$return_date), format = "%m/%d/%Y")-as.Date(as.character(df_medco_join$departure_date), format = "%m/%d/%Y")
+
 # create column for assignment number of first MedCo assignment
 # by getting distinct (first) staff_id from df_medco_join
 # currently df_medco_join has n=946 MedCo assignments (but some staff_id has multiple assignments)
@@ -69,7 +72,9 @@ y$rev_assign_num <- rev(y$rev_assign_num)
 colnames(y)[1] <- "staff_id"
 colnames(y)[2] <- "assign_num_last_medco"
 
-
+# create new data frame, all_assign_medco by inner_join y and assign_first_medco
+all_assign_medco <- assign_first_medco %>%
+    inner_join(y, by = "staff_id")
 
 #------------------------------- ggplot2 way slightly easier
 reverse_staff <- df_medco_join %>%
@@ -92,4 +97,44 @@ reverse_staff$assignment_number <- rev(reverse_staff$assignment_number)
 colnames(reverse_staff)[2] <- 'assign_num_last_medco'
 
 # ---------------------
+
+####### Explore both Medco AND Non-Medco Career Paths #######
+
+# df is the original data set with all staff_id doing multiple assignments 
+# (even some who did NOT do a MedCo assignment)
+# this join allow us to see only staff_id who did at least one MedCo assignment
+# different from all MedCo assignments (n=946)
+# different from unique staff_id who did one MedCo (n=462)
+# this one is showing all MedCo assignments for all staff_id (n=3569)
+
+
+df_join <- df %>%
+    inner_join(multiple_medco, by = "staff_id")
+
+# create vector for total number of assignments (including NON-Medco assignments)
+total_number_assignment <- df_join %>% 
+    group_by(staff_id) %>% 
+    tally(sort = TRUE)
+
+colnames(total_number_assignment)[2] <- 'total_num_assign'
+
+# join all_assign_medco and total_number_assignment
+# this gets all distinct staff_id but with following info
+# num_medco - number of MedCo assignments
+# assignment_num_first_medco
+# assignment_num_last_medco
+# total_num_assign (including non-Medco)
+# for unique staff_id (n = 462)
+all_assignment_join <- all_assign_medco %>% 
+    inner_join(total_number_assignment, by = "staff_id")
+
+# create same ALTERNATE version of all_assignment_join 
+# not just distinct staff_id, but multiple assignments for every staff_id
+# (n = 3569)
+
+all_assignment_join2 <- all_assignment_join %>%
+    select(staff_id, assign_num_first_medco, assign_num_last_medco, total_num_assign)
+
+# create new data frame (n = 3569) with all: num_medco, assign_num_first_medco, assign_num_last_medco, total_num_assign
+df_join_data <- left_join(df_join, all_assignment_join2, by = 'staff_id')
 
