7075a545df97f9ad9275ebd7f39b5d1759e9f841
paulapivat <paul.apivat@gmail.com>
2020-05-09 21:03:04 +0700

    enter point biserial corr for consecutive2 and num_medco and total_num_assign

--- WPA/followup.R
+++ WPA/followup.R
@@ -53,5 +53,40 @@ biserial.cor(b$sex,b$num_medco, level = 1)   # ltm package, point biserial corr
 b_alt <- b
 
 # convert sex into numeric variable
-# re-run point biserial corr
+# insert new column .before = first_departure
+b_alt <- add_column(b_alt, sex2 = NA, .after = 'sex')
+
+# run ifelse loop to fill 0 = F, 1 = M
+b_alt$sex2 <- ifelse(b_alt$sex=='F', 0, 1)
+
+# re-run point biserial corr btwn gender and num_medco
+biserial.cor(b_alt$num_medco,b_alt$sex2, level = 1)
+# [1] 0.01904766
+
+
+# re-run point biserial corr btwn gender and total_num_assign
+biserial.cor(b_alt$total_num_assign,b_alt$sex2, level = 1)
+# [1] 0.01596403
+
+#### Check that point biserial corr between 'consecutive status' is stronger
+#### with num_medco and total_num_assign
+
+# add consecutive2 column
+b_alt[,'consecutive2'] <- NA
+
+# check if =='consecutive', enter 2, else 1
+# did not register 'NA' as 'else'
+# had to do is.na()
+b_alt$consecutive2 <- ifelse(b_alt$consecutive=='consecutive', 2,1)
+b_alt$consecutive2 <- ifelse(is.na(b_alt$consecutive), 1, b_alt$consecutive2)
+
+# run point biserial corr btwn 'consecutive2' status and num_medco
+# moderately high correlation (proving my point)
+biserial.cor(b_alt$num_medco,b_alt$consecutive2, level = 1)
+# [1] 0.655591
+
+# run point biserial corr btwn consecutive2 and total_num_assign
+# moderately high correlation (proving my point)
+biserial.cor(b_alt$total_num_assign,b_alt$consecutive2, level = 1)
+# [1] 0.5021832
 
