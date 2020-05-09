55522bbbeeff57082cded7d4133d434b06a0bf25
paulapivat <paul.apivat@gmail.com>
2020-05-09 17:07:13 +0700

    add followup.R and initial correlations

---
+++ WPA/followup.R
@@ -0,0 +1,43 @@
+### Update: May 9, 2020 ###
+# R version 3.6.3 (2020-02-29) -- "Holding the Windsock" 
+# RStudio version: 1.2.5042
+
+# Navigate and Load data
+#setwd("/Users/paulapivat/Desktop")
+#load(file = "finalsession.RData")
+
+# libraries
+library(tidyverse)
+library(reshape2)
+library(waffle)
+library(patchwork)
+library(moderndive)
+library(networkD3)
+library(rmarkdown)
+library(tinytex)
+
+
+
+### Question 1:
+
+### Question 2:
+
+### Question 3: Causality
+
+# dataframe 'b' contain distinct staff_id (n = 462)
+# contain age_bracket individual belongs in 
+# at time of first assignment (assignment_number==1)
+# check correlation btwn age_bracket and num_medco
+
+# n = 462
+get_correlation(data = b, age_bracket ~ num_medco, na.rm = FALSE)
+
+#         cor
+# -0.02291633
+
+# n = 462
+get_correlation(data = b, age_bracket ~ total_num_assign, na.rm = FALSE)
+
+#          cor
+# -0.05038205
+
